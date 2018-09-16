bf92064adaf84f03ed8d2999c0a1f6105483369a
Spencer Ellinor <zpencerq@users.noreply.github.com>
2018-09-16 00:12:33 -0700

    Update IOS module to follow Android's impl

--- ios/AmplitudeSDK.m
+++ ios/AmplitudeSDK.m
@@ -7,8 +7,95 @@ RCT_EXPORT_MODULE()
 // initializes Amplitude
 RCT_EXPORT_METHOD(initialize:(NSString* )writeKey setTrackSessionEvents:(BOOL) trackSessionEvents)
 {
-     [Amplitude instance].trackingSessionEvents = trackSessionEvents;
+     [self trackSessionEvents: trackSessionEvents];
      [[Amplitude instance] initializeApiKey: writeKey];
+}
+
+RCT_EXPORT_METHOD(trackSessionEvents:(BOOL) track)
+{
+     [Amplitude instance].trackingSessionEvents = track;
+}
+
++ (BOOL) shouldDisable:(NSDictionary *) options withKey:(NSString *) key
+{
+    id val = [options objectForKey: key];
+    return val && ![val boolValue];
+}
+
+RCT_EXPORT_METHOD(setTrackingOptions:(NSDictionary *)options)
+{
+    AMPTrackingOptions *trackingOptions = [AMPTrackingOptions options];
+
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_IDFA]) {
+      trackingOptions = [trackingOptions disableIDFA];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_IDFV]) {
+      trackingOptions = [trackingOptions disableIDFV];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_CARRIER]) {
+      trackingOptions = [trackingOptions disableCarrier];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_CITY]) {
+      trackingOptions = [trackingOptions disableCity];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_COUNTRY]) {
+      trackingOptions = [trackingOptions disableCountry];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_DEVICE_MANUFACTURER]) {
+      trackingOptions = [trackingOptions disableDeviceManufacturer];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_DEVICE_MODEL]) {
+      trackingOptions = [trackingOptions disableDeviceModel];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_DMA]) {
+      trackingOptions = [trackingOptions disableDMA];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_IP_ADDRESS]) {
+      trackingOptions = [trackingOptions disableIPAddress];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_LANGUAGE]) {
+      trackingOptions = [trackingOptions disableLanguage];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_LAT_LNG]) {
+      trackingOptions = [trackingOptions disableLatLng];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_OS_NAME]) {
+      trackingOptions = [trackingOptions disableOSName];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_OS_VERSION]) {
+      trackingOptions = [trackingOptions disableOSVersion];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_PLATFORM]) {
+      trackingOptions = [trackingOptions disablePlatform];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_REGION]) {
+      trackingOptions = [trackingOptions disableRegion];
+    }
+    if ([RNAmplitudeSDK shouldDisable:options withKey:AMP_TRACKING_OPTION_VERSION_NAME]) {
+      trackingOptions = [trackingOptions disableVersionName];
+    }
+
+    [[Amplitude instance] setTrackingOptions:trackingOptions];
+}
+
+RCT_EXPORT_METHOD(enableLocationListening)
+{
+     [[Amplitude instance] enableLocationListening];
+}
+
+RCT_EXPORT_METHOD(disableLocationListening)
+{
+     [[Amplitude instance] disableLocationListening];
+}
+
+RCT_EXPORT_METHOD(useAdvertisingIdForDeviceId)
+{
+     [[Amplitude instance] useAdvertisingIdForDeviceId];
+}
+
+RCT_EXPORT_METHOD(setOffline:(BOOL)offline)
+{
+     [[Amplitude instance] setOffline:offline];
 }
 
 RCT_EXPORT_METHOD(setUserId:(NSString *)userId)
@@ -26,6 +113,15 @@ RCT_EXPORT_METHOD(setOptOut:(BOOL) optOut)
     [[Amplitude instance] setOptOut:optOut];
 }
 
+RCT_REMAP_METHOD(isOptedOut,
+    isOptedOutWithResolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+
+    NSNumber *optOut = [NSNumber numberWithBool:[Amplitude instance].optOut];
+     resolve(optOut);
+}
+
 RCT_EXPORT_METHOD(clearUserProperties)
 {
      [[Amplitude instance] clearUserProperties];
@@ -36,27 +132,42 @@ RCT_EXPORT_METHOD(regenerateDeviceId)
      [[Amplitude instance] regenerateDeviceId];
 }
 
-RCT_EXPORT_METHOD(logEvent:(NSString *)event)
+RCT_REMAP_METHOD(getDeviceId,
+    getDeviceIdWithResolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-     [[Amplitude instance] logEvent: event];
+     resolve([[Amplitude instance] getDeviceId]);
 }
 
-RCT_EXPORT_METHOD(logEventWithProps:(NSString *)eventKey properties:(NSDictionary *)properties)
+RCT_REMAP_METHOD(getSessionId,
+    getSessionIdWithResolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-     [[Amplitude instance] logEvent: eventKey withEventProperties: properties];
+    resolve([NSNumber numberWithLongLong:[[Amplitude instance] getSessionId]]);
 }
 
-RCT_EXPORT_METHOD(logEventWithTimestamp:(NSString *)eventKey timestamp:(nonnull NSNumber *)timestamp properties:(NSDictionary *)properties)
+RCT_EXPORT_METHOD(logEvent:(NSString *)eventType properties:(NSDictionary *)properties options:(NSDictionary *)options)
 {
-     [[Amplitude instance] logEvent: eventKey withEventProperties: properties withGroups: [NSMutableDictionary dictionary] withTimestamp: timestamp outOfSession: NO];
+  [[Amplitude instance] logEvent:eventType
+             withEventProperties:properties
+                      withGroups:[options objectForKey:@"groups"]
+                   withTimestamp:[options objectForKey:@"timestamp"]
+                    outOfSession:[[options objectForKey:@"outOfSession"] boolValue]];
 }
 
-RCT_EXPORT_METHOD(logRevenue:(NSString *)productIdentifier quantity:(int)quantity price:(double)price receipt:(NSString*)receipt)
+RCT_EXPORT_METHOD(logRevenue:(NSString *)productIdentifier quantity:(int)quantity price:(double)price revenueType:(NSString*)revenueType receipt:(NSString*)receipt eventProperties:(NSDictionary*)eventProperties)
 {
      AMPRevenue *revenue = [[[AMPRevenue revenue] setProductIdentifier:productIdentifier] setQuantity: quantity];
      [revenue setPrice:[NSNumber numberWithDouble:price]];
+     [revenue setRevenueType:revenueType];
      [revenue setReceipt:[receipt dataUsingEncoding:NSUTF8StringEncoding]];
+     [revenue setEventProperties:eventProperties];
      [[Amplitude instance] logRevenueV2:revenue];
+}
+
+RCT_EXPORT_METHOD(setGroup:(NSString *)groupType groupName:(NSObject *)groupName)
+{
+     [[Amplitude instance] setGroup:groupType groupName:groupName];
 }
 
 RCT_EXPORT_METHOD(addToUserProperty:(NSString *)property value:(int)value)
