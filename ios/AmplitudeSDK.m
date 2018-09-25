fae31a7327554fb3a38f0f2724452c4de446622e
Spencer Ellinor <zpencerq@users.noreply.github.com>
2018-09-24 22:53:34 -0700

    Format iOS package properly

--- ios/AmplitudeSDK.m
+++ ios/AmplitudeSDK.m
@@ -7,13 +7,13 @@ RCT_EXPORT_MODULE()
 // initializes Amplitude
 RCT_EXPORT_METHOD(initialize:(NSString* )writeKey setTrackSessionEvents:(BOOL) trackSessionEvents)
 {
-     [self trackSessionEvents: trackSessionEvents];
-     [[Amplitude instance] initializeApiKey: writeKey];
+    [self trackSessionEvents: trackSessionEvents];
+    [[Amplitude instance] initializeApiKey: writeKey];
 }
 
 RCT_EXPORT_METHOD(trackSessionEvents:(BOOL) track)
 {
-     [Amplitude instance].trackingSessionEvents = track;
+    [Amplitude instance].trackingSessionEvents = track;
 }
 
 + (BOOL) shouldDisable:(NSDictionary *) options withKey:(NSString *) key
@@ -80,32 +80,32 @@ RCT_EXPORT_METHOD(setTrackingOptions:(NSDictionary *)options)
 
 RCT_EXPORT_METHOD(enableLocationListening)
 {
-     [[Amplitude instance] enableLocationListening];
+    [[Amplitude instance] enableLocationListening];
 }
 
 RCT_EXPORT_METHOD(disableLocationListening)
 {
-     [[Amplitude instance] disableLocationListening];
+    [[Amplitude instance] disableLocationListening];
 }
 
 RCT_EXPORT_METHOD(useAdvertisingIdForDeviceId)
 {
-     [[Amplitude instance] useAdvertisingIdForDeviceId];
+    [[Amplitude instance] useAdvertisingIdForDeviceId];
 }
 
 RCT_EXPORT_METHOD(setOffline:(BOOL)offline)
 {
-     [[Amplitude instance] setOffline:offline];
+    [[Amplitude instance] setOffline:offline];
 }
 
 RCT_EXPORT_METHOD(setUserId:(NSString *)userId)
 {
-     [[Amplitude instance] setUserId:userId];
+    [[Amplitude instance] setUserId:userId];
 }
 
 RCT_EXPORT_METHOD(setUserProperties:(NSDictionary *)properties)
 {
-     [[Amplitude instance] setUserProperties:properties];
+    [[Amplitude instance] setUserProperties:properties];
 }
 
 RCT_EXPORT_METHOD(setOptOut:(BOOL) optOut)
@@ -114,72 +114,72 @@ RCT_EXPORT_METHOD(setOptOut:(BOOL) optOut)
 }
 
 RCT_REMAP_METHOD(isOptedOut,
-    isOptedOutWithResolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+                 isOptedOutWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
 
     NSNumber *optOut = [NSNumber numberWithBool:[Amplitude instance].optOut];
-     resolve(optOut);
+    resolve(optOut);
 }
 
 RCT_EXPORT_METHOD(clearUserProperties)
 {
-     [[Amplitude instance] clearUserProperties];
+    [[Amplitude instance] clearUserProperties];
 }
 
 RCT_EXPORT_METHOD(regenerateDeviceId)
 {
-     [[Amplitude instance] regenerateDeviceId];
+    [[Amplitude instance] regenerateDeviceId];
 }
 
 RCT_REMAP_METHOD(getDeviceId,
-    getDeviceIdWithResolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+                 getDeviceIdWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
-     resolve([[Amplitude instance] getDeviceId]);
+    resolve([[Amplitude instance] getDeviceId]);
 }
 
 RCT_REMAP_METHOD(getSessionId,
-    getSessionIdWithResolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
+                 getSessionIdWithResolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
 {
     resolve([NSNumber numberWithLongLong:[[Amplitude instance] getSessionId]]);
 }
 
 RCT_EXPORT_METHOD(logEvent:(NSString *)eventType properties:(NSDictionary *)properties options:(NSDictionary *)options)
 {
-  [[Amplitude instance] logEvent:eventType
-             withEventProperties:properties
-                      withGroups:[options objectForKey:@"groups"]
-                   withTimestamp:[options objectForKey:@"timestamp"]
-                    outOfSession:[[options objectForKey:@"outOfSession"] boolValue]];
+    [[Amplitude instance] logEvent:eventType
+               withEventProperties:properties
+                        withGroups:[options objectForKey:@"groups"]
+                     withTimestamp:[options objectForKey:@"timestamp"]
+                      outOfSession:[[options objectForKey:@"outOfSession"] boolValue]];
 }
 
 RCT_EXPORT_METHOD(logRevenue:(NSString *)productIdentifier quantity:(int)quantity price:(double)price revenueType:(NSString*)revenueType receipt:(NSString*)receipt eventProperties:(NSDictionary*)eventProperties)
 {
-     AMPRevenue *revenue = [[[AMPRevenue revenue] setProductIdentifier:productIdentifier] setQuantity: quantity];
-     [revenue setPrice:[NSNumber numberWithDouble:price]];
-     [revenue setRevenueType:revenueType];
-     [revenue setReceipt:[receipt dataUsingEncoding:NSUTF8StringEncoding]];
-     [revenue setEventProperties:eventProperties];
-     [[Amplitude instance] logRevenueV2:revenue];
+    AMPRevenue *revenue = [[[AMPRevenue revenue] setProductIdentifier:productIdentifier] setQuantity: quantity];
+    [revenue setPrice:[NSNumber numberWithDouble:price]];
+    [revenue setRevenueType:revenueType];
+    [revenue setReceipt:[receipt dataUsingEncoding:NSUTF8StringEncoding]];
+    [revenue setEventProperties:eventProperties];
+    [[Amplitude instance] logRevenueV2:revenue];
 }
 
 RCT_EXPORT_METHOD(setGroup:(NSString *)groupType groupName:(NSObject *)groupName)
 {
-     [[Amplitude instance] setGroup:groupType groupName:groupName];
+    [[Amplitude instance] setGroup:groupType groupName:groupName];
 }
 
 RCT_EXPORT_METHOD(addToUserProperty:(NSString *)property value:(int)value)
 {
-     AMPIdentify *identify = [[AMPIdentify identify] add:property value:[NSNumber numberWithInt:value]];
-     [[Amplitude instance] identify:identify];
+    AMPIdentify *identify = [[AMPIdentify identify] add:property value:[NSNumber numberWithInt:value]];
+    [[Amplitude instance] identify:identify];
 }
 
 RCT_EXPORT_METHOD(setUserPropertyOnce:(NSString *)property value:(NSString *)value)
 {
-     AMPIdentify *identify = [[AMPIdentify identify] setOnce:property value:value];
-     [[Amplitude instance] identify:identify];
+    AMPIdentify *identify = [[AMPIdentify identify] setOnce:property value:value];
+    [[Amplitude instance] identify:identify];
 }
 
 @end
